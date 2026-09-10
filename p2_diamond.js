4f5092ea6dacf2fb08a3095ab575a3cf043ae1fe
이용민 <bacaren21@naver.com>
2026-09-10 10:45:00 +0900

    p2: diamond

--- p2_diamond.js
+++ p2_diamond.js
@@ -14,3 +14,42 @@ const n = Number(process.argv[2] ?? 5);
 // TODO: print the diamond with for / if and console.log.
 // Hint: for row i, count the spaces and the stars separately.
 // Hint: "*".repeat(3) gives "***".
+
+let blank_num = n/2-1;
+let star_num = 1;
+
+// 정방향 + 중앙
+for(let i=0; i<n/2; i++){
+   for(let b=0; b<blank_num; b++){
+    process.stdout.write(' ');
+   } 
+   for(let s=0; s<star_num; s++){
+    process.stdout.write('*');
+   }
+   console.log();
+
+   blank_num --;
+   star_num +=2;
+
+}
+
+blank_num ++;
+star_num -=2;
+
+// 역방향
+for(let i=n/2 - 1; i>0; i--){
+
+    blank_num ++;
+    star_num -=2;
+
+    for(let b=0; b<blank_num; b++){
+     process.stdout.write(' ');
+    } 
+    for(let s=0; s<star_num; s++){
+     process.stdout.write('*');
+    }
+    console.log();
+
+   
+
+}
